e81d8a6dea28e97b05ff308683cac4299c0b52a4
SharlynBernice <23it1144@stjosephstechnology.ac.in>
2026-07-05 18:58:36 +0530

    Create EmailService.java

---
+++ Module 1 - Design Pattern/4. Implementing the Adapter Pattern/EmailService.java
@@ -0,0 +1,7 @@
+public class EmailService {
+
+    public void sendEmail(String message) {
+        System.out.println("Email sent successfully.");
+        System.out.println("Message: " + message);
+    }
+}
