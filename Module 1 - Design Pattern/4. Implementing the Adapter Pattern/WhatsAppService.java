69c6bc29323ff867b2fa9c1d7735739371e95d68
SharlynBernice <23it1144@stjosephstechnology.ac.in>
2026-07-05 18:59:32 +0530

    Create WhatsAppService.java

---
+++ Module 1 - Design Pattern/4. Implementing the Adapter Pattern/WhatsAppService.java
@@ -0,0 +1,7 @@
+public class WhatsAppService {
+
+    public void sendWhatsAppMessage(String message) {
+        System.out.println("WhatsApp message sent successfully.");
+        System.out.println("Message: " + message);
+    }
+}
